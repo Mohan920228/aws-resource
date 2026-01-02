a1e77ae9b31d09e3939c5e74a99929f19677b449
mohan92 <mohan920228@gmail.com>
2026-01-02 08:13:11 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  default = "ami-068c0051b15cdb816"
+}
+variable "instance_type" {
+  default = "t3a.nano"
+}
+variable "subnet_id" {
+  default = "subnet-0a54e06d228e37fc2"
+}
+variable "vpc_security_group_ids" {
+  default = "sg-0255e9a92d037cd32"
+}
